1d65c19e550ed211929deba1dd7d2d5c957c029d
hana-sabra <hana.sabra11@gmail.com>
2025-06-28 13:24:15 +0300

    random response generation

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import random 
+responses=[
+  "yes,definitely!",
+  "no,not now." ,
+  "ask again later.",
+  "it is certain.",
+  "very doubtful.",
+  "outlook is good",
+  "better not tell you now.",
+  "concentrate and ask again.",    
+]
+def get_random_response():
+    return random.choice(responses)
+
 def get_user_guess():
     while True:
         try:
